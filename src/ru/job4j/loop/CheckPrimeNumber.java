3155c8eb20edfc9afd0c5e2dff0d811b725cd038
Alexey <zhavoronkov52@gmail.com>
2021-02-25 20:54:27 +0300

    поправил checkstyle

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,8 +3,10 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = true;
-        if (number == 0 || number == 1) return false;
-        for(int i = 2; i < number; i++) {
+        if (number == 0 || number == 1) {
+            return false;
+        }
+        for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 prime = false;
                 break;
